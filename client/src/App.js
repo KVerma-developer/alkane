2068e6168c866af78b5a88ee7ff6d37739a8f4dc
Kanhaiya Verma <kanhaiyaverma.developer@gmail.com>
2023-05-26 22:14:59 +0530

    auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
-
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <img src={logo} alt="logo"/>
-    </div>
-  );
+	const user = localStorage.getItem("token");
+
+	return (
+    <>
+		<Routes>
+      
+			{user && <Route path="/" exact element={<Home />} />}
+			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+		</Routes>
+    </>
+	);
 }
 
 export default App;
+
